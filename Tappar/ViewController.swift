023c795f3a850913bed6cf5318518efbc3481a98
arhanbusam <arhan.busam@gmail.com>
2018-04-08 15:59:53 +1000

    UI ready

--- Tappar/ViewController.swift
+++ Tappar/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftScore: UILabel!
+    
+    @IBOutlet weak var rightScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func leftTopButtonPressed(_ sender: UIButton) {
+        
     }
-
-
+    
+    @IBAction func rightTopButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func leftBottomButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rightBottomButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
